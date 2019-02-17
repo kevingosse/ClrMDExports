897b5c40e5ae7a67e3ba6dbb8d4d14f2a3ee610a
Kevin Gosse <krix33@gmail.com>
2019-02-17 14:46:26 +0100

    Change nuget structure

---
+++ nuget/pack.cmd
@@ -0,0 +1,5 @@
+copy "..\src\ClrMDExports\Init.cs" ".\contentFiles\cs\net471"
+copy "..\src\ClrMDExports\Init.cs" ".\contentFiles\cs\netstandard2.0"
+copy "..\src\ClrMDExports\Init.cs" ".\content"
+
+nuget pack
