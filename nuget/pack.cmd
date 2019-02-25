129f5f77e312003cdb7a230c6a0dfb89248db62d
Kevin Gosse <k.gosse@criteo.com>
2019-02-25 16:51:21 +0100

    Fix post-build and pack.cmd

--- nuget/pack.cmd
+++ nuget/pack.cmd
@@ -1,5 +1,5 @@
-copy "..\src\ClrMDExports\Init.cs" ".\contentFiles\cs\net471"
-copy "..\src\ClrMDExports\Init.cs" ".\contentFiles\cs\netstandard2.0"
-copy "..\src\ClrMDExports\Init.cs" ".\content"
+xcopy /R /Y "..\src\ClrMDExports\Init.cs" ".\contentFiles\cs\net471\"
+xcopy /R /Y "..\src\ClrMDExports\Init.cs" ".\contentFiles\cs\netstandard2.0\"
+xcopy /R /Y "..\src\ClrMDExports\Init.cs" ".\content\"
 
 nuget pack
